575c84705bf64efc62b168e8867c6e7bc676c510
Kulpot <kulpotwahu@gmail.com>
2023-07-27 15:12:28 +0800

    Add project files.

---
+++ Generic Constraints SomeGoodUses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+//ref link:https://www.youtube.com/watch?v=PF_LPi_9lqo&list=PLRwVmtr-pp05ETzLYQ8kv1cI5v9ddT8er&index=15
+//
+
+class ClassOne { }
+
+class MainClass
+{
+    //static void TakeA<T>(T arg) where T : IComparable 
+    //static void TakeA<T>(int arg) where T : IComparable
+    //static void TakeA(IComparable arg) { }
+    static void TakeA<T>(T arg) 
+        where T : IComparable 
+        where T : IEnumerable
+    { 
+    }
+    static void TakeA( arg)
+    { 
+    }
+    static void Main()
+    {
+        //TakeA<int>(5);
+        TakeA(5);
+    }
+}
